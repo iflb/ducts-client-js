af312db5655df25b9f4c3eee1d638d11c122379e
Kenshiro Ueda <kenshiro.ueda@gmail.com>
2021-06-08 20:11:34 +0900

    added some tests

--- __tests__/ducts.test.js
+++ __tests__/ducts.test.js
@@ -1,28 +1,53 @@
 const ducts =  require('../lib/ducts')
 
-test('Test Open', testOpenAndClose);
+var errorCount = 0;
+function resetErrorCount() { errorCount = 0; }
+function incrementErrorCount() { ++errorCount }
+function assertNoError() { expect(errorCount).toBe(0); }
+function assertHasError() { expect(errorCount).not.toBe(0); }
+function initializeTest() {
+    return new Promise(resolve => {
+        resetErrorCount();
+        resolve();
+    });
+}
+
+const duct = new ducts.Duct();
+duct._connection_listener.onopen = (event) => {
+    // console.log('[OPEN]', event);
+};
+duct._connection_listener.onclose = (event) => {
+    // console.log('[CLOSE]', event);
+};
+duct._connection_listener.onerror = (event) => {
+    // console.log('[ERROR]', event);
+    incrementErrorCount();
+};
+duct._connection_listener.onmessage = (event) => {
+    // console.log('[MESSAGE]', event);
+};
+duct.event_error_handler = (rid, eid, data, error) => {
+    // console.log(String(rid) + '-' + String(eid) + '-' + String(data) + ': ' + String(error));
+    incrementErrorCount();
+};
 
 const wsd_url = 'https://sdk.ducts.io/ducts/wsd';
 
-var errorCount = 0;
+test(
+    'Test Open And Close',
+    () => {
+        initializeTest()
+            .then(duct.open.bind(null, wsd_url))
+            .then(new Promise(resolve => { duct.close(); resolve(); }))
+            .then(assertNoError);
+    },
+);
 
-const duct = new ducts.Duct();
-duct._connection_listener.onopen = (event) => { console.log('[OPEN]', event); };
-duct._connection_listener.onclose = (event) => { console.log('[CLOSE]', event); };
-duct._connection_listener.onerror = (event) => { console.log('[ERROR]', event); };
-duct._connection_listener.onmessage = (event) => { console.log('[MESSAGE]', event); };
-duct.event_error_handler = (rid, eid, data, error) => {
-    console.log(String(rid) + '-' + String(eid) + '-' + String(data) + ': ' + String(error));
-    ++errorCount;
-};
-
-function resetErrorCount() {
-    errorCount = 0;
-}
-
-async function testOpenAndClose() {
-    resetErrorCount();
-    await duct.open(wsd_url);
-    duct.close();
-    expect(errorCount).toBe(0);
-}
+test(
+    'Test Open Fails',
+    () => {
+        initializeTest()
+            .then(duct.open.bind(null, 'https://invalid_url'))
+            .then(assertHasError);
+    },
+);
